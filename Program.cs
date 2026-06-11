9b852178fcd4b224b1fbcc878f575dbf5ccf5525
Ileana Gonzalez <ileanagonzalez@Ileanas-MacBook-Pro.local>
2026-06-11 00:37:05 -0600

    Greeting: finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lab 5 Mastermind
+// Ileana Gonzalez, 06/10
+
+Console.Clear();
+Console.WriteLine("- Welcome to the Guess the Secret String Game! -\n");
+Console.Write("How long should the string be? ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("How many letters should be used? ");
+int letters = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"I have chosen {length} between 'a' and '{(char)(96 + letters)}' and have arranged hem in a particular order.");
+Console.WriteLine("Your job is to guess the letters and put them in the rigth order.");
--- obj/Debug/net8.0/lab-5-mastermind-Ileanadd.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-5-mastermind-Ileanadd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-5-mastermind-Ileanadd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d12b8fa9ef302dbd21b44d44a0c82132bf57f63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bfbc2e88c796af0b74d57e58c1a68b2dbcdb8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-5-mastermind-Ileanadd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-5-mastermind-Ileanadd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
